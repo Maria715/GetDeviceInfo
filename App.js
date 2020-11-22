c9a4cdb36b4ae2be04355b57680ddc6bdfb2410e
Maria Akhtar <mariarajakimail@gmail.com>
2020-11-22 19:18:30 +0500

    Initial Comit

--- App.js
+++ App.js
@@ -1,21 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
+import { getMacAddressAsync } from 'expo-network';
+import NetInfo from '@react-native-community/netinfo';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
+const App = () => {
+  const [netInfo, setNetInfo] = useState('');
+  const [MacAddress, setMacAddress]= useState('')
+  useEffect(() => {
+
+     (async () => {
+      getMacAddressAsync().then(v => {
+        console.log("Mac Address",v);
+         setMacAddress(v)
+      });
+    })();
+
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setNetInfo(
+        `Connection type: ${state.type}
+        Is connected?: ${state.isConnected}
+        IP Address: ${state.details.ipAddress}
+         Device Brand Name: ${Device.brand}
+         Device Id: ${Constants.deviceId}
+          Installation Id: ${Constants.installationId}
+           Device Name: ${Constants.deviceName}`
+      );
+    });
+
+    return () => {
+
+      unsubscribe();
+    };
+  }, []);
+
+  const getNetInfo = () => {
+    // To get the network state once
+    NetInfo.fetch().then((state) => {
+      console.log(Device.brand)
+      console.log("Device Id",Constants.deviceId)
+      
+
+      alert(
+
+        `Connection type: ${state.type}
+        Is connected?: ${state.isConnected}
+        IP Address: ${state.details.ipAddress}`
+      );
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          React Native NetInfo
+          {'\n'}
+          To Get NetInfo information
+        </Text>
+        <Text style={styles.textStyle}>
+          {/*Here is NetInfo to get device type*/}
+          {netInfo}
+          <Text>{"\nMac Address " +MacAddress}</Text>
+        </Text>
+        <Button title="Get more detailed NetInfo" onPress=
+        {getNetInfo
+        } />
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
     justifyContent: 'center',
   },
+  header: {
+    fontSize: 22,
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+  textStyle: {
+    marginTop: 30,
+    fontSize: 16,
+    textAlign: 'center',
+    color: 'black',
+    paddingVertical: 20,
+  },
 });
+
+export default App;
